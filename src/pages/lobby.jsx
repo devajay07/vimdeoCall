fc243ba6cc80588793c5157ad3740471c16c01ea
Ajay Yadav <ayyadav179@gmail.com>
2023-07-10 16:57:03 +0530

    home page & fixed git issues

--- src/pages/lobby.jsx
+++ src/pages/lobby.jsx
@@ -1,236 +1,200 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./lobby.css";
 import Agora from "agora-rtm-sdk";
 
 function LobbyPage() {
-  const peerConnectionRef = useRef(null);
-  const localStreamRef = useRef(null);
-  const remoteStreamRef = useRef(null);
+  let peerConnection;
+  const servers = {
+    iceServers: [
+      {
+        urls: "stun:stun.l.google.com:19302",
+      },
+    ],
+  };
+
+  let localStream;
   const appId = "ea43bb1b76644e2288285733f6510fee";
   let token = null;
   let uid = String(Math.floor(Math.random() * 10000));
   let client;
   let channel;
+  let remoteStream;
 
-  useEffect(() => {
-    const servers = {
-      iceServers: [
-        {
-          urls: "stun:stun.l.google.com:19302",
-        },
-      ],
-    };
+  const init = async () => {
+    client = Agora.createInstance(appId);
 
-    const init = async () => {
-      client = Agora.createInstance(appId);
+    await client.login({ uid, token });
 
-      await client.login({ uid, token });
+    channel = client.createChannel("main");
+    await channel.join();
 
-      channel = client.createChannel("main");
-      await channel.join();
+    channel.on("MemberJoined", handleMemberJoined);
+    channel.on("MemberLeft", handleMemberLeft);
 
-      channel.on("MemberJoined", handleMemberJoined);
-      channel.on("MemberLeft", handleMemberLeft);
+    client.on("MessageFromPeer", handleMessageFromPeer);
 
-      client.on("MessageFromPeer", handleMessageFromPeer);
-
-      try {
-        localStreamRef.current = await navigator.mediaDevices.getUserMedia({
-          video: true,
-          audio: true,
-        });
-        document.getElementById("user-1").srcObject = localStreamRef.current;
-      } catch (error) {
-        console.log("Error accessing media devices:", error);
-      }
-
-      navigator.permissions.query({ name: "camera" }).then(function (result) {
-        if (result.state === "granted") {
-          console.log("Camera access granted");
-        } else if (result.state === "prompt") {
-          console.log("Camera access prompt");
-        } else if (result.state === "denied") {
-          console.log("Camera access denied");
-        }
+    if (navigator.mediaDevices) {
+      localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
       });
+    } else {
+      // handle errorl
+      console.log("no api");
+    }
 
-      document
-        .getElementById("camera-btn")
-        .addEventListener("click", toggleCamera);
-      document.getElementById("mic-btn").addEventListener("click", toggleAudio);
-    };
-
-    const handleMemberJoined = async (memberId) => {
-      console.log("handleMemberJoined called with memberId:", memberId);
-      createOffer(memberId);
-    };
-
-    const handleMessageFromPeer = async (message, memberId) => {
-      message = JSON.parse(message.text);
-
-      if (message.type === "offer") createAnswer(memberId, message.offer);
-
-      if (message.type === "answer") addAnswer(message.answer);
-
-      if (message.type === "candidate") {
-        if (peerConnectionRef.current)
-          peerConnectionRef.current.addIceCandidate(message.candidate);
+    navigator.permissions.query({ name: "camera" }).then(function (result) {
+      if (result.state === "granted") {
+        // camera access granted
+        console.log("y");
+      } else if (result.state === "prompt") {
+        // camera access not yet granted, show a prompt to ask for permission
+        console.log("ask");
+      } else if (result.state === "denied") {
+        // camera access denied, show an error message
+        console.log("n");
       }
-    };
+    });
 
-    const handleMemberLeft = () => {
-      document.getElementById("user-2").style.display = "none";
-      cleanUpResources();
-    };
+    document.getElementById("user-1").srcObject = localStream;
 
-    const createPeerConnection = async (memberId) => {
-      peerConnectionRef.current = new RTCPeerConnection(servers);
+    document
+      .getElementById("camera-btn")
+      .addEventListener("click", toggleCamera);
+    document.getElementById("mic-btn").addEventListener("click", toggleAudio);
+  };
 
-      remoteStreamRef.current = new MediaStream();
-      document.getElementById("user-2").srcObject = remoteStreamRef.current;
+  const handleMemberJoined = async (MemeberId) => {
+    console.log("handleMemberJoined called with memberId:", MemeberId);
+    createOffer(MemeberId);
+  };
 
-      document.getElementById("user-2").style.display = "block";
+  const handleMessageFromPeer = async (message, MemeberId) => {
+    message = JSON.parse(message.text);
 
-      if (!localStreamRef.current) {
-        try {
-          localStreamRef.current = await navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true,
-          });
-          document.getElementById("user-1").srcObject = localStreamRef.current;
-        } catch (error) {
-          console.log("Error accessing media devices:", error);
-        }
-      }
+    if (message.type === "offer") createAnswer(MemeberId, message.offer);
 
-      localStreamRef.current.getTracks().forEach((track) => {
-        peerConnectionRef.current.addTrack(track, localStreamRef.current);
+    if (message.type === "answer") addAnswer(message.answer);
+
+    if (message.type === "candidate") {
+      if (peerConnection) peerConnection.addIceCandidate(message.candidate);
+    }
+  };
+
+  const handleMemberLeft = () => {
+    document.getElementById("user-2").style.display = "none";
+  };
+
+  const createPeerConnection = async (MemeberId) => {
+    peerConnection = new RTCPeerConnection(servers);
+
+    remoteStream = new MediaStream();
+    document.getElementById("user-2").srcObject = remoteStream;
+
+    document.getElementById("user-2").style.display = "block";
+
+    if (!localStream) {
+      localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
       });
+      document.getElementById("user-1").srcObject = localStream;
+    }
 
-      peerConnectionRef.current.ontrack = (event) => {
-        event.streams[0].getTracks().forEach((track) => {
-          remoteStreamRef.current.addTrack(track);
-        });
-      };
+    localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, localStream);
+    });
 
-      peerConnectionRef.current.onicecandidate = async (event) => {
-        if (event.candidate) {
-          client.sendMessageToPeer(
-            {
-              text: JSON.stringify({
-                type: "candidate",
-                candidate: event.candidate,
-              }),
-            },
-            memberId
-          );
-        }
-      };
+    peerConnection.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+      });
     };
 
-    const createOffer = async (memberId) => {
-      await createPeerConnection(memberId);
-
-      try {
-        const offer = await peerConnectionRef.current.createOffer();
-        await peerConnectionRef.current.setLocalDescription(offer);
-
+    peerConnection.onicecandidate = async (event) => {
+      if (event.candidate) {
         client.sendMessageToPeer(
-          { text: JSON.stringify({ type: "offer", offer: offer }) },
-          memberId
+          {
+            text: JSON.stringify({
+              type: "candidate",
+              candidate: event.candidate,
+            }),
+          },
+          MemeberId
         );
-      } catch (error) {
-        console.log("Error creating offer:", error);
       }
     };
+  };
 
-    const createAnswer = async (memberId, offer) => {
-      await createPeerConnection(memberId);
+  const createOffer = async (MemeberId) => {
+    await createPeerConnection(MemeberId);
 
-      try {
-        await peerConnectionRef.current.setRemoteDescription(offer);
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
 
-        const answer = await peerConnectionRef.current.createAnswer();
-        await peerConnectionRef.current.setLocalDescription(answer);
+    client.sendMessageToPeer(
+      { text: JSON.stringify({ type: "offer", offer: offer }) },
+      MemeberId
+    );
+  };
 
-        client.sendMessageToPeer(
-          { text: JSON.stringify({ type: "answer", answer: answer }) },
-          memberId
-        );
-      } catch (error) {
-        console.log("Error creating answer:", error);
-      }
-    };
+  const createAnswer = async (MemeberId, offer) => {
+    await createPeerConnection(MemeberId);
 
-    const addAnswer = async (answer) => {
-      if (!peerConnectionRef.current.currentRemoteDescription)
-        peerConnectionRef.current.setRemoteDescription(answer);
-    };
+    await peerConnection.setRemoteDescription(offer);
 
-    const toggleCamera = () => {
-      const videoTrack = localStreamRef.current
-        .getTracks()
-        .find((track) => track.kind === "video");
+    let answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
 
-      if (videoTrack.enabled) {
-        videoTrack.enabled = false;
-        document.getElementById("camera-btn").style.backgroundColor = "#ff202b";
-      } else {
-        videoTrack.enabled = true;
-        document.getElementById("camera-btn").style.backgroundColor =
-          "rgb(179,102,249,0.9)";
-      }
-    };
+    client.sendMessageToPeer(
+      { text: JSON.stringify({ type: "answer", answer: answer }) },
+      MemeberId
+    );
+  };
 
-    const toggleAudio = () => {
-      const audioTrack = localStreamRef.current
-        .getTracks()
-        .find((track) => track.kind === "audio");
-
-      if (audioTrack.enabled) {
-        audioTrack.enabled = false;
-        document.getElementById("mic-btn").style.backgroundColor = "#ff202b";
-      } else {
-        audioTrack.enabled = true;
-        document.getElementById("mic-btn").style.backgroundColor =
-          "rgb(179,102,249,0.9)";
-      }
-    };
-
-    const cleanUpResources = () => {
-      if (peerConnectionRef.current) {
-        peerConnectionRef.current.close();
-        peerConnectionRef.current = null;
-      }
-
-      if (localStreamRef.current) {
-        localStreamRef.current.getTracks().forEach((track) => {
-          track.stop();
-        });
-        localStreamRef.current = null;
-      }
-
-      if (remoteStreamRef.current) {
-        remoteStreamRef.current.getTracks().forEach((track) => {
-          track.stop();
-        });
-        remoteStreamRef.current = null;
-      }
-    };
-
-    window.addEventListener("beforeunload", leaveChannel);
-
-    init();
-
-    return () => {
-      cleanUpResources();
-    };
-  }, []);
+  const addAnswer = async (answer) => {
+    if (!peerConnection.currentRemoteDescription)
+      peerConnection.setRemoteDescription(answer);
+  };
 
   const leaveChannel = async () => {
     await channel.leaveChannel();
     await client.logout();
+  }; // 30 seconds delay ????
+
+  const toggleCamera = () => {
+    const videoTrack = localStream
+      .getTracks()
+      .find((track) => track.kind === "video");
+
+    if (videoTrack.enabled) {
+      videoTrack.enabled = false;
+      document.getElementById("camera-btn").style.backgroundColor = "#ff202b";
+    } else {
+      videoTrack.enabled = true;
+      document.getElementById("camera-btn").style.backgroundColor =
+        "rgb(179,102,249,0.9)";
+    }
   };
+
+  const toggleAudio = () => {
+    const audioTrack = localStream
+      .getTracks()
+      .find((track) => track.kind === "audio");
+
+    if (audioTrack.enabled) {
+      audioTrack.enabled = false;
+      document.getElementById("mic-btn").style.backgroundColor = "#ff202b";
+    } else {
+      audioTrack.enabled = true;
+      document.getElementById("mic-btn").style.backgroundColor =
+        "rgb(179,102,249,0.9)";
+    }
+  };
+
+  window.addEventListener("beforeunload", leaveChannel);
+
+  init();
 
   return (
     <div className="App">
@@ -249,6 +213,7 @@ function LobbyPage() {
           playsInline
         ></video>
       </div>
+
       <div id="controls">
         <div id="camera-controls">
           <div className="camera-control-btn" id="camera-btn">
